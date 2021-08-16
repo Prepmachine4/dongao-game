062d31eacb29f210a7f151d4437a902155bca844
Prepmachine4 <724130843@qq.com>
2021-08-16 16:03:47 +0800

    bug fix

--- miniprogram/pages/study/study.js
+++ miniprogram/pages/study/study.js
@@ -107,16 +107,3 @@ Page({
     }
   }
 })
-
-const app = getApp();
-Page({
-  data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar
-  },
-  isCard(e) {
-    this.setData({
-      isCard: e.detail.value
-    })
-  },
-});
